395f65831c3b98f86744cae13f7b3f58bfe18a66
shaunalynn <shaunalynn@duffles.(none)>
2009-05-07 08:47:40 -0400

    created first functional and technical spec for btg. next steps are fleshing out a sprout-style spec and scheduling todos.

---
+++ bigger_than_government.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+
+The Thorn Handbook is a web application to facilitate
+location-based activist activity. This first iteration will allow for
+the creation of geo-tagged nodes on a map representing the location of
+resources--whether physical, social, or otherwise--which can then be
+manipulated through the map interface or viewed in more detail. 
+
+Additionally, it will allow for the creation of 'narrative nodes,'
+stories of an activist's experience, contextualizing the discovery or
+use of a resource.
+
+Functionally, the first iteration will ::
+- ability to geotag resources and bits of narrative
+- ability to create filtered view of map
+- ability to create filtered view of narrative 
+- ability to search site content
+
+## user interface
+### screens for ui
+- home page
+- node creation
+- map view
+- narrative view
+- detailed node view
+
+## technical functionality
+### function to instantiate map
+### function to create node on map
+### function to move node 
+### function to delete node
+### function to tag node
+### function to filter nodes
+### function to search tags and text
+### function to construct narrative
+### function to attach media to node
+
+## media
+### function to display photo from flickr
+### function to embed video from youtube
+### function to display pdf
+"""
